8fa2faf3b339a056809a3a43723d93a305c5e926
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-07-09 07:46:12 -0300

    create styled and navigation

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
+import { StyleSheet } from "react-native";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
